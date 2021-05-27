75315fa741608d94e048e49f586bfbc841bc6f92
lucas-trubiano <lucas.trubiano@gmail.com>
2021-05-27 20:18:13 -0300

    save all

---
+++ SupermercadoConsola/aplicacion.py
@@ -0,0 +1,17 @@
+def Agregar(productos):
+	
+	# tener una variable donde guardar
+	# pedir el dato que quiero agregar
+	# validarlo
+	# guardar en su lugar
+	print("Se agrego tu producto")
+
+def Eliminar():
+	print("Se ha eliminado tu producto")
+
+def Ver(productos):
+	#print(productos)
+	print("Nombre","Cantidad")
+	for p in productos:
+		print(p)
+	#print("Tenes 5 productos")
--- SupermercadoConsola/main.py
+++ SupermercadoConsola/main.py
@@ -1,1 +1,34 @@
 # Archivo Principal de la Aplicacion
+from aplicacion import Agregar,Ver,Eliminar
+
+# Supermercado
+lista_productos = [	["pan",3],
+					["gaseosa",2]	]
+
+horario = {	"Lunes":[10,18],
+			"Martes":[14,18],
+			"Miercoles":[10,18]	}
+for key in horario:
+	print(f"Los {key} hace horarios de {horario[key][0]} a {horario[key][1]}")
+
+menu = """========== MENU ==========
+1) Agregar producto
+2) Eliminar producto
+3) Ver productos
+4) Salir"""
+
+while True:
+	print(menu)
+	opc = int(input(">> "))
+
+	if opc==1:
+		Agregar()
+	elif opc==2:
+		Eliminar()
+	elif opc ==3:
+		Ver(lista_productos)
+	elif opc==4:
+		print("Adios")
+		break
+	else:
+		print("opcion incorrecta elija otra")
